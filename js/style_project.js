71cbd0cfd24a677c9e5535b15343e863e024b014
virza805 <virza805@gmail.com>
2019-08-27 10:34:18 +0600

    complit 1st psd markp & css withut hover

---
+++ js/style_project.js
@@ -0,0 +1,15 @@
+(function ($) {
+    "use strict";
+    
+    jQuery(document).ready(function($){
+
+      
+        $(".product-list").masonry();
+
+    });
+
+    jQuery(window).load(function(){
+
+    });
+
+}(jQuery));
