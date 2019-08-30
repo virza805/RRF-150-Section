2ad1a3da25fa567070eaddfeca9b8b6f88eb4928
virza805 <virza805@gmail.com>
2019-08-30 09:31:51 +0600

    1st project compleat in RRF>150 (10-17) But carolole Ni

--- js/style_project.js
+++ js/style_project.js
@@ -2,9 +2,32 @@
     "use strict";
     
     jQuery(document).ready(function($){
-
+        // $(".client-testimonial").owlCarousel({
+        //     items:3,
+        //     margin:30,
+        //     nav:false,
+        //     dots: true,
+        //     loop: true
+        // });
       
         $(".product-list").masonry();
+
+        // $(".homepage-slides").owlCarousel({
+        //     items:1,
+        //     nav:true,
+        //     dots: false,
+        //     autoplay: false,
+        //     loop: true,
+        //     navText:["<i class='fa fa-long-arrow-left'></i>", "<i class='fa fa-long-arrow-right'></i>"]
+        // });
+        $(".menu-trigger").on("click", function(){
+            $("#off-canvar-menu, .off-canvar-overlay").addClass("active");
+            return false;
+        });
+        $(".menu-close, .off-canvar-overlay").on("click", function(){
+            $("#off-canvar-menu, .off-canvar-overlay").removeClass("active");
+           
+        });
 
     });
 
