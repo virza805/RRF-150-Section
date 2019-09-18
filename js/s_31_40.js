65581e2cd8da4e8c27e979540a3b4cd221981e9c
virza805 <virza805@gmail.com>
2019-09-19 02:32:59 +0600

    S_31-40 Section complete without wole carosol

---
+++ js/s_31_40.js
@@ -0,0 +1,19 @@
+(function ($) {
+    "use strict";
+    
+    jQuery(document).ready(function($){
+
+        var s = skrollr.init({
+            render:function(data){
+                //Debugging - Log the current scroll position
+                //console.log(data.curTop);
+            }
+        });
+
+    });
+
+    jQuery(window).load(function(){
+
+    });
+
+}(jQuery));
