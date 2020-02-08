cc7b09c2a23fc894c3b285e47e32698430c81e29
virza805 <virza805@gmail.com>
2020-02-08 08:53:19 +0600

    RRF Complete without s_58,59,60,61 & 79  API

--- js/s_123_126.js
+++ js/s_123_126.js
@@ -2,13 +2,26 @@
     "use strict";
     
     jQuery(document).ready(function($){
-        $(".owl-carousel").owlCarousel({
-            items:1,
-            loop:true,
-            autoplay:false,
-            nav:false,
-            dots:true
+        $(".portfoli-list").masonry({
+            
         });
+        $(".single-portfolio-item").hover(function(){
+            $(".single-portfolio-item .portfolio-hover h2").removeClass("animated slideInDown");
+            $(this).find(".portfolio-hover h2").addClass("animated slideInDown");
+            $(".single-portfolio-item .portfolio-hover p").removeClass("animated bounce");
+            $(this).find(".portfolio-hover p").addClass("animated bounce");
+        });
+        // this is off canvs menu on
+        $(".menu-btn").on('click', function(){
+            $(".offcanvs-menu").addClass("active");
+            $(".offcanvs-menu-overlay").addClass("active");
+        });
+        $(".menu-close i.fa, .offcanvs-menu-overlay").on('click', function(){
+            $(".offcanvs-menu").removeClass("active");
+            $(".offcanvs-menu-overlay").removeClass("active");
+        });
+        // smart header use jquery
+        $(".header-area").headroom();
 
     });
 
